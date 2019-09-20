d7d33ea9ce9ea48fa5bbe84663c65807a1e8f3bc
John Reyes <john.jreye@gmail.com>
2019-09-20 18:26:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |name|
+    new_array.push(badge_maker(name))
+  end
+  new_array
+end
